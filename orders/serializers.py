70b4764fa6aee9d65fc367a3a8e65a4417429158
Bita <bitafarezi79@gmail.com>
2026-07-09 01:40:03 +0330

    Create extrafield with SerializerMethodField

--- orders/serializers.py
+++ orders/serializers.py
@@ -10,6 +10,22 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    
+    # we dont have this feild in database but we want to show to user
+    delivery_duration = serializers.SerializerMethodField()
+    
+    def get_delivery_duration(self, obj):
+        if obj.status == 'Pending':
+            return "45-60 minutes"
+        
+        elif obj.status == 'Preparing':
+            return "30-45 minutes"
+        
+        elif obj.status == 'Out for Delivery':
+            return "10-20 minutes"
+        
+        return "Delivered"
+    
     class Meta:
         model = Order
         fields = '__all__'

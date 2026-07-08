dcb935ede59661b6359124529b52a46c851839c6
Bita <bitafarezi79@gmail.com>
2026-07-09 00:58:39 +0330

    Create UserProfile model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
+
+User = get_user_model()
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user')
+    ROLE_CHOICES = (
+        ('customer', 'Customer'),
+        ('driver', 'Driver'),
+        ('restaurant', 'Restaurant'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    phone_number = models.CharField(max_length=11)
+    address = models.TextField()
+    

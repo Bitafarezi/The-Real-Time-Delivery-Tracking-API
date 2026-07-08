a68f6a9a084c427b9b480bcaf9d8d459e99ba0b5
Bita <bitafarezi79@gmail.com>
2026-07-09 01:07:39 +0330

    Create Order model

--- orders/models.py
+++ orders/models.py
@@ -16,4 +16,20 @@ class UserProfile(models.Model):
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
     phone_number = models.CharField(max_length=11)
     address = models.TextField()
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    driver = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='deliveries', null=True, blank=True)
+    restaurant_name = models.CharField(max_length=20)
+    address = models.TextField() 
     
+    STATUS_CHOICES = (
+        ('Pending', 'Pending'),
+        ('Preparing', 'Preparing'),
+        ('Out for Delivery', 'Out for Delivery'),
+        ('Delivered', 'Delivered'),
+    )
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)

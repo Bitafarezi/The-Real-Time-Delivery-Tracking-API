d2a139ff72c82ec032a2b9c625c964b4fa246744
Bita <bitafarezi79@gmail.com>
2026-07-09 00:33:32 +0330

    Introduce new apps to INSTALLED_APPS

--- delivery_backend/settings.py
+++ delivery_backend/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'orders.apps.OrdersConfig',
+    'rest_framework',
+    'django_filters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

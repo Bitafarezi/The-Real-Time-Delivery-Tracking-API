12c7d2f948ae318928d249d55f33411ba04e1fc7
Bita <bitafarezi79@gmail.com>
2026-07-09 01:10:31 +0330

    Debug UserProfile model

--- orders/models.py
+++ orders/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     ROLE_CHOICES = (
         ('customer', 'Customer'),
         ('driver', 'Driver'),

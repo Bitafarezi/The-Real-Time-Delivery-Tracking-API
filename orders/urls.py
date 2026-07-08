69ac791d38d7b49bc0b58114f9b3830c3a1c26f6
Bita <bitafarezi79@gmail.com>
2026-07-09 02:10:59 +0330

    Create and register router based on SimpleRouter

---
+++ orders/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+
+from .views import OrderViewSet
+
+
+router = SimpleRouter()
+router.register('order', OrderViewSet, basename='order')
+

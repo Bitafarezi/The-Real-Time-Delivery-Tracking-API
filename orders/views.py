8a72e299ac6c118429dcc6f1495d56a1f053da04
Bita <bitafarezi79@gmail.com>
2026-07-09 02:06:56 +0330

    Filter orders based on the role a user has

--- orders/views.py
+++ orders/views.py
@@ -6,6 +6,19 @@ from .serializers import OrderSerializer
 
 # With ModelViewSet you get GET, POST, PUT/PATCH, DELETE ready to use
 class OrderViewSet(ModelViewSet):
-    queryset = Order.objects.all()
+    # queryset = Order.objects.all()
+    
+    # filter orders based on the role a user has
+    def get_queryset(self):
+        user = self.request.user
+        
+        if user.profile.role == "customer":
+            return Order.objects.filter(customer=user)
+        
+        elif user.profile.role == "driver":
+            return Order.objects.filter(driver=user)
+        
+        return Order.objects.none()
+    
     serializer_class = OrderSerializer
 

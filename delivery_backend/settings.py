8d1d297323845dbe2b1330b52d6b52bcb130f747
Bita <bitafarezi79@gmail.com>
2026-07-16 21:21:40 +0330

    Configure path of static directory

--- delivery_backend/settings.py
+++ delivery_backend/settings.py
@@ -118,3 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Path of our static directory
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"

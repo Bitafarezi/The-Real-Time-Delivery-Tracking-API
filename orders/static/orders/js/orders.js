8ea7aaf2c7fcdb61e86633c51b0f8f97e848b3ae
Bita <bitafarezi79@gmail.com>
2026-07-16 21:46:10 +0330

    Fetch all orders (Get request)

--- orders/static/orders/js/orders.js
+++ orders/static/orders/js/orders.js
@@ -2,7 +2,7 @@
 
 // Function to get Django's CSRF Token for safe AJAX requests
 function getCookie(name) {
-    let cookie_value = null;
+    let cookieValue = null;
     if (document.cookie && document.cookie !== '') {
         const cookies = document.cookie.split(';');
         
@@ -11,9 +11,28 @@ function getCookie(name) {
 
             if (cookie.substring(0, name.length + 1) === (name + '=')) {
                 
-                cookie_value = decodeURIComponent(cookie.substring(name.length + 1));
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                 break;
             }
         }
     }
+    return cookieValue
+}
+
+// 1. Fetch all orders (Get request)
+async function loadOrders(params) {
+    try {
+        const response = await fetch(BASE_URL);
+        if (!response.ok) throw new Error('Failed to fetch orders');
+        const orders = await response.json();
+        renderOrders(orders);
+    } catch (error) {
+        console.error(error);
+        document.getElementById('orders-container').innerHTML = `
+            <div class="col-span-full text-center py-12 text-red-500 font-semibold">
+                Error loading orders. Please check your login session!
+            </div>
+        `;
+    }
+    
 }

39b75ea115e4a1fcc6f02c75c579a59e51486f0f
Bita <bitafarezi79@gmail.com>
2026-07-16 21:30:45 +0330

    Add Function to get Django's CSRF Token for safe AJAX requests

---
+++ orders/static/orders/js/orders.js
@@ -0,0 +1,19 @@
+// static/orders/js/orders.js
+
+// Function to get Django's CSRF Token for safe AJAX requests
+function getCookie(name) {
+    let cookie_value = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                
+                cookie_value = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+}
